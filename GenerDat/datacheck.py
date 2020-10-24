6575087378d091d1d69985c0aca98d7175142eee
sadegh <sadegh6khan@protonmail.ch>
2020-10-24 18:27:31 +0330

    added num_class(), added docstrings.

--- GenerDat/datacheck.py
+++ GenerDat/datacheck.py
@@ -3,6 +3,7 @@ import pprint
 
 
 def sum_list(json_path):
+    """Show how much of each letter we have."""
     char_list = {}
     with open(json_path) as f:
         x = json.load(f)
@@ -15,10 +16,23 @@ def sum_list(json_path):
                     char_list[i] = 1
     char_list['sum'] = sum(char_list.values())
     pp.pprint(char_list)
+    return None
+
+
+def num_class(json_path):
+    """Show total number of classes in the data."""
+    with open(json_path) as f:
+        x = json.load(f)
+        chars = []
+        for block in x:
+            chars.extend(block['parts'])
+    # print(set(chars))
+    return len(set(chars))
 
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=2)
     # json_file = '/home/sadegh/Projects/OCR/datasets/medaya/final-pretty.json'
-    json_file = '/home/sadegh/Projects/OCR/datasets/dgen2/v40/final-pretty.json'
+    json_file = '/home/sadegh/Projects/OCR/datasets/data4/final.json'
     sum_list(json_file)
+    print('Total number of classes is:', num_class(json_file))
