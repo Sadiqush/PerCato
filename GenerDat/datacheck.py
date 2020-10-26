e25b2db416ceee96d5192fe1d3741b2539da3f86
sadegh <sadegh6khan@protonmail.ch>
2020-10-26 18:25:38 +0330

    added compat_check() and fe_check()

--- GenerDat/datacheck.py
+++ GenerDat/datacheck.py
@@ -1,5 +1,7 @@
 import json
 import pprint
+import textutil
+import re
 
 
 def sum_list(json_path):
@@ -30,9 +32,27 @@ def num_class(json_path):
     return len(set(chars))
 
 
+def compat_check(newalph):
+    """Check the compatibility of unicodes with the old ALPHABET used."""
+    count = 0
+    for c in textutil.ALPHABET:
+        if c in newalph:
+            count += 1
+    if count == 33:
+        print("It's compatible.")
+    else:
+        print("Only %d are compatible." % count)
+
+def fe_check(checkstr):
+    """See all the chars in a list as unicode."""
+    res = (re.sub('.', lambda x: r'\u % 04X' % ord(x.group()), checkstr))
+    return res
+
+
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=2)
-    # json_file = '/home/sadegh/Projects/OCR/datasets/medaya/final-pretty.json'
-    json_file = '/home/sadegh/Projects/OCR/datasets/data4/final.json'
+    json_file = '/home/sadegh/Projects/OCR/datasets/data8.1/final.json'
     sum_list(json_file)
     print('Total number of classes is:', num_class(json_file))
+    newalph = textutil.TextGen.get_join_alphabet(view=False)
+    compat_check(newalph)
