c4fc70cb846a9f8ccc4fb072490d2f22b7d28299
abavafa <abavafa@live.com>
2020-11-03 10:15:14 +0330

    CharacterManager improved

--- GenerDat/characterutil.py
+++ GenerDat/characterutil.py
@@ -2,6 +2,8 @@ import json
 from enum import IntFlag
 from typing import Union, Dict
 import random
+
+
 # from timeit import timeit
 
 
@@ -51,38 +53,34 @@ class PersianLetter(Character):
             return False
         return True
 
+    def __copy__(self):
+        c = PersianLetter('\0')
+        c.__dict__ = self.__dict__.copy()
+        return c
+
 
 class CharacterManager:
     def __init__(self, json_path: str = "letters.json"):
-        self._letters_map: Dict[str, PersianLetter] = self.load_persian_letters(json_path)
+        self._letter_map: Dict[str, PersianLetter] = self.load_persian_letters(json_path)
+        self._letter_forms = None
 
     def get_persian_letters(self, as_dict=False):
-        return self._letters_map.copy() if as_dict else list(self._letters_map)
+        return {letter.character: letter.__copy__() for letter in self._letter_map.values()} if as_dict \
+            else list(self._letter_map.keys())
+
+    def get_persian_letter_forms(self):
+        if self._letter_forms is None:
+            forms = set()
+            forms.update(*((v.initial_form, v.medial_form, v.final_form, v.isolated_form)
+                           for v in self._letter_map.values()))
+            if None in forms:
+                forms.remove(None)
+            self._letter_forms = list(forms)
+        return self._letter_forms.copy()
 
     @staticmethod
-    def get_persian_letters_ugly(view=False):
-        """Create an alphabet that consists of all forms of letters, in ugly way."""
-        letters = ['ﺍ', 'ﺎ', 'ﺏ', 'ﺐ', 'ﺑ', 'ﺒ', 'ﺕ', 'ﺖ', 'ﺗ', 'ﺘ', 'ﺙ',
-                   'ﺚ', 'ﺛ', 'ﺜ', 'ﺝ', 'ﺞ', 'ﺟ', 'ﺠ', 'ﺡ', 'ﺢ', 'ﺣ', 'ﺤ',
-                   'ﺥ', 'ﺦ', 'ﺧ', 'ﺨ', 'ﺩ', 'ﺪ', 'ﺫ', 'ﺬ', 'ﺭ', 'ﺮ', 'ﺯ',
-                   'ﺰ', 'ﺱ', 'ﺲ', 'ﺳ', 'ﺴ', 'ﺵ', 'ﺶ', 'ﺷ', 'ﺸ', 'ﺹ', 'ﺺ',
-                   'ﺻ', 'ﺼ', 'ﺽ', 'ﺾ', 'ﺿ', 'ﻀ', 'ﻁ', 'ﻂ', 'ﻅ', 'ﻆ', 'ﻉ',
-                   'ﻊ', 'ﻋ', 'ﻌ', 'ﻍ', 'ﻎ', 'ﻏ', 'ﻐ', 'ﻑ', 'ﻒ', 'ﻓ', 'ﻔ',
-                   'ﻕ', 'ﻖ', 'ﻗ', 'ﻘ', 'ﮎ', 'ﻚ', 'ﻛ', 'ﻜ', 'ﻝ', 'ﻞ', 'ﻟ',
-                   'ﻠ', 'ﻡ', 'ﻢ', 'ﻣ', 'ﻤ', 'ﻥ', 'ﻦ', 'ﻧ', 'ﻨ', 'ﻩ', 'ﻪ',
-                   'ﻫ', 'ﻬ', 'ﻭ', 'ﻮ', 'ﯼ', 'ﭖ', 'ﭗ', 'ﭘ', 'ﭙ', 'ﭺ', 'ﭻ',
-                   'ﭼ', 'ﭽ', 'ژ', 'ﮋ', 'ﮒ', 'ﮓ', 'ﮔ', 'ﮕ', 'ﺃ', 'ﺋ', 'ﺊ',
-                   'ﺌ', 'ﺉ', 'ﺁ', 'ؤ', 'لا']
-        if view:
-            print("the alphabet is a total of %s:" % len(letters))
-            for i in letters:
-                print(f"{i}\t", end="")
-
-        return letters
-
-    @staticmethod
-    def load_persian_letters(json_path):
-        with open('letters.json', 'r', encoding="utf-8") as file:
+    def load_persian_letters(json_path='letters.json'):
+        with open(json_path, 'r', encoding="utf-8") as file:
             letters_dict_list = json.load(file)["letters"]
         letters = {}
         for letter_dict in letters_dict_list:
@@ -91,12 +89,12 @@ class CharacterManager:
             letters[letter.character] = letter
         return letters
 
-    def fix_letters(self, string: str, reject_unknown=False):
+    def fix_letters(self, string: str, throw_unknown=False):
         fixed = []
         for i, c in enumerate(string):
-            if c in self._letters_map:
+            if c in self._letter_map:
                 c = self.get_letter_form(string, i)
-            elif reject_unknown:
+            elif throw_unknown:
                 raise Exception(f"Unknown letter '{c}' in \"{string}\".")
             fixed.append(c)
         return "".join(fixed)
@@ -106,40 +104,27 @@ class CharacterManager:
         c = string[index]
         if index < len(string) - 1:
             cf = string[index + 1]
-            if cf in self._letters_map and \
-                    self._letters_map[c].can_connect(PersianLetterSide.FRONT) and \
-                    self._letters_map[cf].can_connect(PersianLetterSide.BACK):
+            if cf in self._letter_map and \
+                    self._letter_map[c].can_connect(PersianLetterSide.FRONT) and \
+                    self._letter_map[cf].can_connect(PersianLetterSide.BACK):
                 flag |= PersianLetterSide.FRONT
         if index > 0:
             cb = string[index - 1]
-            if cb in self._letters_map and \
-                    self._letters_map[c].can_connect(PersianLetterSide.BACK) and \
-                    self._letters_map[cb].can_connect(PersianLetterSide.FRONT):
+            if cb in self._letter_map and \
+                    self._letter_map[c].can_connect(PersianLetterSide.BACK) and \
+                    self._letter_map[cb].can_connect(PersianLetterSide.FRONT):
                 flag |= PersianLetterSide.BACK
-        return self._letters_map[c].get_connected_form(flag)
+        return self._letter_map[c].get_connected_form(flag)
 
-    def get_equal_words(self, length: int, batch: int, ugly=True):
+    def get_equal_words(self, length: int, batch: int, ugly=True, seed=None):
         """Generate random words with equal weight (probability) for letters"""
-        if ugly:
-            letters = self.get_persian_letters_ugly()
-        else:
-            letters = [letter.character for letter in self._letters_map.values()]
-
-        words = []
-        # random.seed(42)
-        for i in range(batch):
-            word = ''.join(random.choices(letters, k=length, weights=[1] * len(letters)))
-            words.append(word)
-        # print(words)
-
+        if seed:
+            random.seed(seed)
+        letters = self.get_persian_letter_forms() if ugly else self.get_persian_letters()
+        words = list({''.join(random.choices(letters, k=length)) for _ in range(batch)})
         return words
-
-    @property
-    def letters_map(self):
-        return self._letters_map
 
 
 if __name__ == '__main__':
     cm = CharacterManager()
-    x = cm.fix_letters('صادق')
-    print(list(x))
+    print(cm.get_equal_words(10, 3, ugly=False))
