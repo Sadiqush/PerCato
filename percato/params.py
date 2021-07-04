2a74225b84a96840dcdc22295c9368b3333d2a43
sadiqu <sadiqush@gmail.com>
2021-07-04 21:12:33 +0430

    changed letter path to percato

--- percato/params.py
+++ percato/params.py
@@ -2,19 +2,19 @@ from pathlib import Path
 
 batch = 10
 length = (3, 6)
-is_meaningful = False   # Buggy
-ugly_mode = True
+is_meaningful = True   # Buggy
+ugly_mode = False
 using_mask = False
 loosebox = False
 save_with_detectron_format = False
 
 im_sadiqu = 1
 if im_sadiqu:
-    image_path = Path.home() / "Projects/OCR/datasets/data17/train_images"
+    image_path = Path.home() / "percat_images"
     json_path = str((image_path.parent / "train_ocr.json").absolute())
     letters_path = str((image_path.parent / "used_letters").absolute())
     image_path = str(image_path.absolute())
-    ocr_path = Path.home() / 'PycharmProjects/ocrdg/'
+    ocr_path = Path.home() / 'PycharmProjects/PerCato/'
     font_path = str((ocr_path / "b_nazanin.ttf").absolute())
 else:
     image_path = "images/"
